675626bb7691f5ff767777cf6aa6e2fd51ec6db7
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2025-08-05 10:03:02 -0400

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,18 +8,18 @@ echo "--- Running post-create script ---"
 
 # Activating the virtual environment
 echo "Creating virtual environment..."
-python3 -m venv /workspaces/erasmus-gp/.venv
+python3 -m venv /workspace/erasmus-gp/.venv
 
 # Install Python dependencies from requirements.txt
 echo "Installing requirements..."
 /workspaces/erasmus-gp/.venv/bin/pip install --upgrade pip
 find . -name "requirements.txt" -exec ./.venv/bin/pip install -r {} \;
-/workspaces/erasmus-gp/.venv/bin/pip install -e ./egpcommon
-/workspaces/erasmus-gp/.venv/bin/pip install -e ./egppkrapi
-/workspaces/erasmus-gp/.venv/bin/pip install -e ./egppy
-/workspaces/erasmus-gp/.venv/bin/pip install -e ./egpdb
-/workspaces/erasmus-gp/.venv/bin/pip install -e ./egpseed
-/workspaces/erasmus-gp/.venv/bin/pip install -e ./egpdbmgr
+/workspace/erasmus-gp/.venv/bin/pip install -e ./egpcommon
+/workspace/erasmus-gp/.venv/bin/pip install -e ./egppkrapi
+/workspace/erasmus-gp/.venv/bin/pip install -e ./egppy
+/workspace/erasmus-gp/.venv/bin/pip install -e ./egpdb
+/workspace/erasmus-gp/.venv/bin/pip install -e ./egpseed
+/workspace/erasmus-gp/.venv/bin/pip install -e ./egpdbmgr
 
 # Done
 echo "--- Post-create script finished ---"
